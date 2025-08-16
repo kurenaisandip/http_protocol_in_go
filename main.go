c5ff6e111f7fc04c978c2e671f69ff97770a2187
Sandip <srai68141@gmail.com>
2025-08-16 15:08:31 +0545

    feat: updated the logic to read through all the data from the file and break the loop if there is nothing read in the file

--- main.go
+++ main.go
@@ -15,7 +15,25 @@ func check(err error) {
 func main() {
 
 	//read file from the disk
-	dat, err := os.ReadFile("./message.txt")
+	//dat, err := os.ReadFile("./message.txt") // this method also work but i cannot control how to parse the file
+	//check(err)
+	//fmt.Println(string(dat))
+
+	//for more control i am going to use .open method
+	file, err := os.Open("./message.txt")
 	check(err)
-	fmt.Println(string(dat))
+
+	buffer := make([]byte, 8)
+
+	for {
+		n1, err := file.Read(buffer)
+		if n1 > 0 {
+			fmt.Printf("%d bytes: %s\n", n1, string(buffer[:n1]))
+		}
+
+		if err != nil {
+			break
+		}
+	}
+
 }
