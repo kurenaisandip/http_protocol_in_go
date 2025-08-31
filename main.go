647306f0b88ac4688a7dc9c5e313967ccaddd5c6
Sandip <srai68141@gmail.com>
2025-08-31 21:54:52 +0545

    feat: moved the code from reading from file to reading from the client request

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	_ "net"
 	_ "os"
+	"strings"
 )
 
 // method to catch the error
@@ -15,47 +17,47 @@ func check(err error) {
 }
 
 // Reads 8 bytes at a time, aggregates lines, and sends them to a channel
-//func getLinesChannel(f io.ReadCloser) <-chan string {
-//	ch := make(chan string)   // channel to return
-//	buffer := make([]byte, 8) // 8-byte buffer
-//	var currentLine string    // holds incomplete line across chunks
-//
-//	go func() {
-//		defer close(ch) // close channel when done
-//		defer f.Close() // close file when done
-//
-//		//for {
-//		//	n, err := net.Listen(buffer) //f.Read(buffer)
-//		//	if n > 0 {
-//		//		chunk := string(buffer[:n])
-//		//		parts := strings.Split(chunk, "\n")
-//		//
-//		//		for i := 0; i < len(parts)-1; i++ {
-//		//			// send full line to channel
-//		//			ch <- currentLine + parts[i]
-//		//			currentLine = ""
-//		//		}
-//		//
-//		//		// last part may be incomplete, keep for next iteration
-//		//		currentLine += parts[len(parts)-1]
-//		//	}
-//		//
-//		//	if err != nil {
-//		//		if err != io.EOF {
-//		//			fmt.Println("Error reading file:", err)
-//		//		}
-//		//		break
-//		//	}
-//		//}
-//
-//		// send any leftover line after EOF
-//		if len(currentLine) > 0 {
-//			ch <- currentLine
-//		}
-//	}()
-//
-//	return ch
-//}
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)   // channel to return
+	buffer := make([]byte, 8) // 8-byte buffer
+	var currentLine string    // holds incomplete line across chunks
+
+	go func() {
+		defer close(ch) // close channel when done
+		//defer f.Close() // close file when done
+
+		for {
+			n, err := f.Read(buffer)
+			if n > 0 {
+				chunk := string(buffer[:n])
+				parts := strings.Split(chunk, "\n")
+
+				for i := 0; i < len(parts)-1; i++ {
+					// send full line to channel
+					ch <- currentLine + parts[i]
+					currentLine = ""
+				}
+
+				// last part may be incomplete, keep for next iteration
+				currentLine += parts[len(parts)-1]
+			}
+
+			if err != nil {
+				if err != io.EOF {
+					fmt.Println("Error reading file:", err)
+				}
+				break
+			}
+		}
+
+		// send any leftover line after EOF
+		if len(currentLine) > 0 {
+			ch <- currentLine
+		}
+	}()
+
+	return ch
+}
 
 func main() {
 
@@ -89,4 +91,8 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	fmt.Println("new connection has been accepted:", conn.RemoteAddr())
+
+	for line := range getLinesChannel(conn) { // read lines from the client
+		fmt.Printf("received: %s\n", line)
+	}
 }
